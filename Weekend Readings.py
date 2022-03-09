b95a9d7960562d7e1638fbc6b3cbc8747353964c
Luca <luca.villani1@gmail.com>
2022-03-09 17:54:50 +0100

    added welcome text and other details

--- Weekend Readings.py
+++ Weekend Readings.py
@@ -4,8 +4,9 @@ from urllib.parse import quote
 
 import webbrowser
 
+# TARGET WEBSITE
 url = urlopen('https://collectingwisdom.com/')
-
+# FAVOURITE BLOGS TO RETRIEVE
 keywords = ["monevator", "accidentalfire", "financialsamurai"]
 
 webbrowser.register('chrome', None, webbrowser.BackgroundBrowser("C:\Program Files\Google\Chrome\Application\chrome.exe"))
@@ -18,7 +19,8 @@ html = url.read()
 html_decoded = html.decode("utf-8")
 final_text = html_decoded.split()
 
-
+print("\n WELCOME TO WEEKEND READINGS \n \n \n Please find below the latest articles from your favorite personal finance bloggers: \n")
+# RETRIEVE AND PRINT THE ARTICLES
 for line in final_text:
     if keywords[(0 + x)] in line:
         clean_line = line.replace("href=", "")
@@ -28,14 +30,16 @@ for line in final_text:
     if x >= len(keywords):
         break
 
-
+print("")
+# DO YOU WANT TO OPEN THE ARTICLES?
 read = input("Do you want to read those articles? [y/n] ")
 if read == "y":
     for article in article_list:
         article = article_list[0 + y]
         article_unquoted = article.replace('"', '')
         y = y + 1
-        print(article_unquoted)
         webbrowser.get('chrome').open((article_unquoted))
+    print("\n \n Thank you! You will be shortly redirected to your browser \n \n")
 
 
+#NEXT STEPS: RETRVIE ARTICLE TITLES
